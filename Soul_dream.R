381eb1cc79cbe8a9a90d299f253480c2c8e1a4e8
Jacomijn Prins <jacomijnprins@gmail.com>
2023-03-06 12:00:48 +0100

    The best version

---
+++ Soul_dream.R
@@ -0,0 +1,7 @@
+soul_mix <- get_playlist_audio_features("", "37i9dQZF1EQntZpEGgfBif")
+Two_trees <- get_track_audio_features("64jxwV2kklovj7ldPsPsXF")
+Midnight <- get_track_audio_features("2YtCVNJbORhMnxTtIaj6fk")
+
+# similar_Lianne <- get_related_artists("2RP4pPHTXlQpDnO9LvR7Yt")
+#tracks <- get_album_tracks("5A5ePoPwlIuxVmn3cIhiFw")
+#similar_album <- get_recommendations("7vF5RIx2jHtt9Y0OElOZKK")
